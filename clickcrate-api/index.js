7fdab6c28fbba0a0981b337ac1142ee5285b390f
adrian-petrykiw <cucumber901@gmail.com>
2024-05-18 09:59:05 -0500

    init

--- clickcrate-api/index.js
+++
@@ -1,6 +0,0 @@
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "http://localhost:3000";
-
-app.listen(PORT, () => {
-  console.log(`ClickCrate API server listening on ${HOST}`);
-});
